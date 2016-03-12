181d33892a57a4dfca780013f555a4d8f3c6261b
Daniele Sciacchitano <dan@metadigit.it>
2016-03-12 19:44:57 +0100

    refactor [acl] xml config & internal workflow

--- src/acl/ACL.php
+++ src/acl/ACL.php
@@ -12,29 +12,43 @@ use metadigit\core\Kernel,
 class ACL {
 	use \metadigit\core\CoreTrait, \metadigit\core\db\PdoTrait;
 
-	/** DB settings
+	const SQL_CHECK_ROUTE	= 'SELECT * FROM %s WHERE type = "URL" AND ( method IS NULL OR method = :method ) ORDER BY CHAR_LENGTH(target) DESC';
+	const SQL_CHECK_OBJECT	= 'SELECT * FROM %s WHERE type = "OBJECT" AND target = :target AND ( method IS NULL OR method = :method )';
+	const SQL_CHECK_ORM		= 'SELECT * FROM %s WHERE type = "ORM" AND target = :target AND ( method IS NULL OR method = :method )';
+
+	const SQL_MATCH_ACTION_USER		= 'SELECT COUNT(*) FROM %s_actions_2_users WHERE action_id = :action_id AND user_id = :user_id';
+	const SQL_MATCH_ACTION_GROUP	= 'SELECT COUNT(*) FROM %s_actions_2_groups WHERE action_id = :action_id AND group_id IN ( SELECT group_id FROM %s WHERE user_id = :user_id )';
+	const SQL_MATCH_FILTER_USER		= 'SELECT val FROM %s_filters_2_users WHERE filter_id = :filter_id AND user_id = :user_id';
+	const SQL_MATCH_FILTER_GROUP	= 'SELECT val FROM %s_filters_2_groups WHERE filter_id = :filter_id AND group_id IN ( SELECT group_id FROM %s WHERE user_id = :user_id )';
+
+	const SQL_FETCH_ACTION = 'SELECT name FROM %s_actions WHERE id = %u';
+	const SQL_FETCH_FILTER = 'SELECT name FROM %s_filters WHERE id = %u';
+	const SQL_FETCH_QUERY = 'SELECT query FROM %s_filters_sql WHERE id = %u';
+
+
+	/** DB tables
 	 * @var array */
-	protected $dbConf = [
-		't_prefix'	=> 'sys_acl',
-		't_users'	=> 'sys_users',
-		't_groups'	=> 'sys_groups',
-		't_u2g'		=> 'sys_users_2_groups'
+	protected $tables = [
+		'acl'	=> 'sys_acl',
+		'users'	=> 'sys_users',
+		'groups'=> 'sys_groups',
+		'u2g'	=> 'sys_users_2_groups'
 	];
 
 	/**
 	 * ACL constructor.
 	 * @param string $pdo PDO instance ID
-	 * @param array $dbConf DB settings
+	 * @param array $tables DB tables
 	 */
-	function __construct($pdo, array $dbConf) {
+	function __construct($pdo, array $tables) {
 		$this->pdo = $pdo;
-		$this->dbConf = $dbConf;
+		$this->tables = $tables;
 		TRACE and $this->trace(LOG_DEBUG, 1, __METHOD__, 'initialize ACL storage');
 		$PDO = Kernel::pdo($pdo);
 		$driver = $PDO->getAttribute(\PDO::ATTR_DRIVER_NAME);
 		$PDO->exec(str_replace(
 			['acl', 't_u2g', 't_users', 't_groups'],
-			[$dbConf['t_prefix'], $dbConf['t_u2g'], $dbConf['t_users'], $dbConf['t_groups']],
+			[$tables['acl'], $tables['u2g'], $tables['users'], $tables['groups']],
 			file_get_contents(__DIR__.'/sql/init-'.$driver.'.sql')
 		));
 	}
@@ -43,10 +57,8 @@ class ACL {
 		$target = $Req->URI();
 		$method =$Req->getMethod();
 		$acl = null;
-		$aclArray = $this->pdoStExecute(
-			'SELECT * FROM '.$this->dbConf['t_prefix'].' WHERE type = "URL" AND method = :method ORDER BY CHAR_LENGTH(target) DESC',
-			['method'=>$method]
-		)->fetchAll(\PDO::FETCH_ASSOC);
+		$aclArray = $this->pdoStExecute(sprintf(self::SQL_CHECK_ROUTE, $this->tables['acl']),
+			['method'=>$method])->fetchAll(\PDO::FETCH_ASSOC);
 		foreach($aclArray as $item) {
 			$item['target'] = str_replace('/', '\\/', $item['target']);
 			if(preg_match('/'.$item['target'].'/', $target)) {
@@ -54,92 +66,83 @@ class ACL {
 				break;
 			}
 		}
-		return ($acl) ? $this->checkAcl($acl) : true;
+		if($acl && !empty($acl['action'])) $this->checkAction($acl);
+		if($acl && !empty($acl['filter'])) $this->checkFilter($acl);
+		return true;
 	}
 
 	function onObject($target, $method) {
-		$acl = $this->pdoStExecute(
-			'SELECT * FROM '.$this->dbConf['t_prefix'].' WHERE type = "OBJECT" AND target = :target AND method = :method',
-			['target'=>$target, 'method'=>$method]
-		)->fetch(\PDO::FETCH_ASSOC);
-		return ($acl) ? $this->checkAcl($acl) : true;
+		$acl = $this->pdoStExecute(sprintf(self::SQL_CHECK_OBJECT, $this->tables['acl']),
+			['target'=>$target, 'method'=>$method])->fetch(\PDO::FETCH_ASSOC);
+		if($acl && !empty($acl['action'])) $this->checkAction($acl);
+		if($acl && !empty($acl['filter'])) $this->checkFilter($acl);
+		return true;
 	}
 
 	function onOrm($target, $method) {
-		$acl = $this->pdoStExecute(
-			'SELECT * FROM '.$this->dbConf['t_prefix'].' WHERE type = "ORM" AND target = :target AND method = :method',
-			['target'=>$target, 'method'=>$method]
-		)->fetch(\PDO::FETCH_ASSOC);
-		return ($acl) ? $this->checkAcl($acl) : true;
+		$acl = $this->pdoStExecute(sprintf(self::SQL_CHECK_ORM, $this->tables['acl']),
+			['target'=>$target, 'method'=>$method])->fetch(\PDO::FETCH_ASSOC);
+		if($acl && !empty($acl['action'])) $this->checkAction($acl);
+		if($acl && !empty($acl['filter'])) $this->checkFilter($acl);
+		return true;
 	}
 
-	protected function checkAcl(array $acl) {
-		TRACE and $this->trace(LOG_DEBUG, 1, __METHOD__, "$acl[type] $acl[target] $acl[method]");
-//echo "\n UID: $_SESSION[UID] - ACL: $acl[type] $acl[target] $acl[method] \n";
-		if($acl['action']) {
-			$actionName = $this->pdoQuery('SELECT name FROM '.$this->dbConf['t_prefix'].'_actions WHERE id = '.$acl['action'])->fetchColumn();
-			if(
-				!$this->pdoStExecute(
-					'SELECT COUNT(*) FROM '.$this->dbConf['t_prefix'].'_actions_2_users WHERE action_id = :action_id AND user_id = :user_id',
-					['action_id'=>$acl['action'], 'user_id'=>$_SESSION['UID']]
-				)->fetchColumn()
-				&&
-				!$this->pdoStExecute(
-					'SELECT COUNT(*) FROM '.$this->dbConf['t_prefix'].'_actions_2_groups WHERE action_id = :action_id AND group_id IN (SELECT group_id FROM '.$this->dbConf['t_u2g'].' WHERE user_id = :user_id )',
-					['action_id'=>$acl['action'], 'user_id'=>$_SESSION['UID']]
-				)->fetchColumn()
-			) {
+	protected function checkAction(array $acl) {
+		$actionName = $this->pdoQuery(sprintf(self::SQL_FETCH_ACTION, $this->tables['acl'], $acl['action']))->fetchColumn();
+		if(
+			!$this->pdoStExecute(sprintf(self::SQL_MATCH_ACTION_USER, $this->tables['acl']),
+				['action_id'=>$acl['action'], 'user_id'=>$_SESSION['UID']])->fetchColumn()
+			&&
+			!$this->pdoStExecute(sprintf(self::SQL_MATCH_ACTION_GROUP, $this->tables['acl'], $this->tables['u2g']),
+				['action_id'=>$acl['action'], 'user_id'=>$_SESSION['UID']])->fetchColumn()
+		) {
 //echo "\t ACTION [$acl[action]] $actionName => EXCEPTION 100 \n";
-				throw new Exception(100, [$actionName]);
-			} else {
+			throw new Exception(100, [$actionName]);
+		} else {
+			TRACE and $this->trace(LOG_DEBUG, 1, __METHOD__, "$acl[type] $acl[target] $acl[method] - ACTION: $actionName => OK ");
 //echo "\t ACTION [$acl[action]] $actionName => OK \n";
-			}
 		}
-		if($acl['filter']) {
-			$filterName = $this->pdoQuery('SELECT name FROM '.$this->dbConf['t_prefix'].'_filters WHERE id = '.$acl['filter'])->fetchColumn();
-			$values1 = (array) $this->pdoStExecute(
-				'SELECT val FROM '.$this->dbConf['t_prefix'].'_filters_2_users WHERE filter_id = :filter_id AND user_id = :user_id',
-				['filter_id'=>$acl['filter'], 'user_id'=>$_SESSION['UID']]
-			)->fetchAll(\PDO::FETCH_COLUMN);
+	}
 
-			$values2 = (array) $this->pdoStExecute(
-				'SELECT val FROM '.$this->dbConf['t_prefix'].'_filters_2_groups WHERE filter_id = :filter_id AND group_id IN (SELECT group_id FROM '.$this->dbConf['t_u2g'].' WHERE user_id = :user_id )',
-				['filter_id'=>$acl['filter'], 'user_id'=>$_SESSION['UID']]
-			)->fetchAll(\PDO::FETCH_COLUMN);
+	protected function checkFilter(array $acl) {
+		$filterName = $this->pdoQuery(sprintf(self::SQL_FETCH_FILTER, $this->tables['acl'], $acl['filter']))->fetchColumn();
+		$values1 = (array) $this->pdoStExecute(sprintf(self::SQL_MATCH_FILTER_USER, $this->tables['acl']),
+			['filter_id'=>$acl['filter'], 'user_id'=>$_SESSION['UID']])->fetchAll(\PDO::FETCH_COLUMN);
 
-			$values = array_merge($values1, $values2);
+		$values2 = (array) $this->pdoStExecute(sprintf(self::SQL_MATCH_FILTER_GROUP, $this->tables['acl'], $this->tables['u2g']),
+			['filter_id'=>$acl['filter'], 'user_id'=>$_SESSION['UID']])->fetchAll(\PDO::FETCH_COLUMN);
+
+		$values = array_merge($values1, $values2);
 //var_dump($values);
-			if(empty($values)) {
+		if(empty($values)) {
 //echo "\t FILTER [$acl[filter]] $filterName => EXCEPTION 200 \n";
-				throw new Exception(200, [$filterName]);
-			} elseif(array_search('*', $values) !== false) {
+			throw new Exception(200, [$filterName]);
+		} elseif(array_search('*', $values) !== false) {
+			TRACE and $this->trace(LOG_DEBUG, 1, __METHOD__, "$acl[type] $acl[target] $acl[method] - FILTER: $filterName VALUE: * => OK ");
 //echo "\t FILTER [$acl[filter]] $filterName * => OK \n";
-				return true;
-			} else {
+			return true;
+		} else {
 //echo "\t FILTER [$acl[filter]] $filterName VALUES: ".implode(' ', $values)." \n";
-			$query = $this->pdoQuery('SELECT query FROM '.$this->dbConf['t_prefix'].'_filters_sql WHERE id = '.$acl['filter_sql'])->fetchColumn();
+			$query = $this->pdoQuery(sprintf(self::SQL_FETCH_QUERY, $this->tables['acl'], $acl['filter_sql']))->fetchColumn();
 //echo "\t QUERY: $query \n";
 
-				// parse qyery params
-				$params = null;
+			// parse qyery params
+			$params = null;
 
-				// execute
-				switch($acl['type']) {
-					case 'ORM':
-						return true;
-						break;
-					default:
-						if($r = $this->pdoStExecute($query, $params)->fetchColumn()) {
+			// execute
+			switch($acl['type']) {
+				case 'ORM':
+					return true;
+					break;
+				default:
+					if($r = $this->pdoStExecute($query, $params)->fetchColumn()) {
 //echo "\t QUERY $r => OK \n";
-							return true;
-						} else {
+						return true;
+					} else {
 //echo "\t QUERY $r => EXCEPTION 201 \n";
-							throw new Exception(201, [$filterName, $query]);
-						}
-				}
+						throw new Exception(201, [$filterName, $query]);
+					}
 			}
-			return false;
 		}
-		return true;
 	}
 }
