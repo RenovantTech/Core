b48d7147353e3a5e25a058106e5026297d77b14b
Daniele Sciacchitano <dan@metadigit.it>
2016-11-20 11:24:48 +0100

    minor [trace] Tracer handlers test

--- tests/trace/TracerTest.php
+++ tests/trace/TracerTest.php
@@ -1,9 +1,8 @@
 <?php
 namespace test\tracer;
-use const metadigit\core\trace\T_INFO;
+use const metadigit\core\trace\{T_ERROR, T_INFO};
 use function metadigit\core\trace;
-use metadigit\core\Kernel,
-	metadigit\core\trace\Tracer;
+use metadigit\core\trace\Tracer;
 
 class TracerTest extends \PHPUnit_Framework_TestCase {
 
@@ -25,21 +24,31 @@ class TracerTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	function testOnError() {
-		file_put_contents(\metadigit\core\LOG_DIR.'system.log', '');
-		Tracer::onError(E_USER_NOTICE, 'trigger NOTICE', __FILE__, __LINE__, null);
-		Tracer::onError(E_USER_ERROR, 'trigger ERROR', __FILE__, __LINE__, null);
-		Kernel::logFlush();
-		$lines = file(\metadigit\core\LOG_DIR.'system.log', FILE_IGNORE_NEW_LINES);
-		$this->assertStringEndsWith('[ERR] kernel: trigger NOTICE - FILE: '.\metadigit\core\BASE_DIR.'trace/TracerTest.php:29', $lines[1]);
-		$this->assertStringEndsWith('[ERR] kernel: trigger ERROR - FILE: '.\metadigit\core\BASE_DIR.'trace/TracerTest.php:30', $lines[2]);
+		trigger_error('NOTICE msg', E_USER_NOTICE);
+		trigger_error('ERROR msg', E_USER_ERROR);
+		$trace = Tracer::export();
+
+		$t = array_pop($trace);
+		$this->assertEquals(LOG_ERR, $t[2]);
+		$this->assertEquals(T_ERROR, $t[3]);
+		$this->assertEquals('E_USER_ERROR', $t[4]);
+		$this->assertEquals('ERROR msg', $t[5]);
+
+		$t = array_pop($trace);
+		$this->assertEquals(LOG_ERR, $t[2]);
+		$this->assertEquals(T_ERROR, $t[3]);
+		$this->assertEquals('E_USER_NOTICE', $t[4]);
+		$this->assertEquals('NOTICE msg', $t[5]);
 	}
 
 	function testOnException() {
-		file_put_contents(\metadigit\core\LOG_DIR.'system.log', '');
 		$Ex = new \Exception('test', 123);
 		Tracer::onException($Ex);
-		Kernel::logFlush();
-		$lines = file(\metadigit\core\LOG_DIR.'system.log', FILE_IGNORE_NEW_LINES);
-		$this->assertStringEndsWith('[ERR] kernel: Exception[123]: test -  - FILE: '.\metadigit\core\BASE_DIR.'trace/TracerTest.php:39', $lines[3]);
+		$trace = Tracer::export();
+		$t = array_pop($trace);
+		$this->assertEquals(LOG_ERR, $t[2]);
+		$this->assertEquals(T_ERROR, $t[3]);
+		$this->assertEquals('Exception', $t[4]);
+		$this->assertEquals('[CODE 123] test', $t[5]);
 	}
 }
