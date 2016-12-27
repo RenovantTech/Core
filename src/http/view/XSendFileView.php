ba794fec6874559007e9b51a806ed62b580873ef
Daniele Sciacchitano <dan@metadigit.it>
2016-12-27 22:13:47 +0100

    refactor [http] XSendFileView->render() options: fileName

--- src/http/view/XSendFileView.php
+++ src/http/view/XSendFileView.php
@@ -22,10 +22,10 @@ class XSendFileView implements \metadigit\core\http\ViewInterface {
 	function render(Request $Req, Response $Res, $resource=null, array $options=null) {
 		if(!file_exists($resource)) throw new Exception(201, ['X-SendFile', $resource]);
 		trace(LOG_DEBUG, T_INFO, 'file: '.$resource);
-		$saveName = $Res->get('saveName') ?: pathinfo($resource, PATHINFO_FILENAME);
+		$fileName = $options['fileName'] ?? pathinfo($resource, PATHINFO_FILENAME);
 		$Res->reset();
 		header('Content-Type: '.((new \finfo(FILEINFO_MIME_TYPE))->file($resource)));
-		header('Content-Disposition: attachment; filename='.$saveName.'.'.pathinfo($resource, PATHINFO_EXTENSION));
+		header('Content-Disposition: attachment; filename='.$fileName.'.'.pathinfo($resource, PATHINFO_EXTENSION));
 		switch($_SERVER['SERVER_SOFTWARE']) {
 			case (strpos($_SERVER['SERVER_SOFTWARE'], 'nginx')!==false):
 				$resource = str_replace(\metadigit\core\PUBLIC_DIR, '/', $resource);
