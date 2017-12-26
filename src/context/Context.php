d4e9617a54e284a7dac9d3d36fec2e8208e381e5
Daniele Sciacchitano <dan@metadigit.it>
2017-12-26 14:08:09 +0100

    refactor [context] Context child Container & EventDispatcher, init()

--- src/context/Context.php
+++ src/context/Context.php
@@ -10,7 +10,11 @@ use const metadigit\core\trace\{T_DEPINJ};
 use metadigit\core\sys,
 	metadigit\core\CoreProxy,
 	metadigit\core\container\Container,
-	metadigit\core\container\ContainerException;
+	metadigit\core\container\ContainerException,
+	metadigit\core\container\ContainerYamlParser,
+	metadigit\core\event\EventDispatcher,
+	metadigit\core\event\EventDispatcherException,
+	metadigit\core\event\EventYamlParser;
 /**
  * Context
  * @author Daniele Sciacchitano <dan@metadigit.it>
@@ -22,73 +26,52 @@ class Context {
 	const FAILURE_EXCEPTION	= 1;
 	const FAILURE_SILENT	= 2;
 
-	/** instantiated contexts
+	/** Container instance
+	 * @var Container */
+	protected $Container;
+	/** EventDispatcher instance
+	 * @var EventDispatcher */
+	protected $EventDispatcher;
+	/** initialized namespaces
 	 * @var array */
-	static protected $_instances = [];
-
-	/**
-	 * Factory method to build a Context
-	 * @param string $namespace Context namespace
-	 * @param boolean $useCache default TRUE, set FALSE to rebuild Context from XML skipping system cache
-	 * @return Context
-	 */
-	static function factory($namespace, $useCache=true) {
-		if($useCache && isset(self::$_instances[$namespace]))
-			return self::$_instances[$namespace];
-		elseif($useCache && $Context = sys::cache('sys')->get($namespace.'.Context'))
-			return self::$_instances[$namespace] = $Context;
-		else {
-			sys::trace(LOG_DEBUG, T_DEPINJ, $namespace, null, __METHOD__);
-			self::$_instances[$namespace] = $Context = new Context($namespace);
-			sys::cache('sys')->set($namespace.'.Context', $Context);
-			return $Context;
-		}
-	}
-
-	/** Map of available objects & their classes
-	 * @var array */
-	protected $id2classMap = [];
-	/** Included Contexts namespaces
-	 * @var array */
-	protected $includedNamespaces = [];
-	/** registered listeners (callbacks)
-	 * @var array */
-	protected $listeners = [];
-	/** Context namespace
-	 * @var string */
-	protected $namespace;
+	protected $namespaces = [];
 	/** Array of instantiated objects (to avoid replication)
 	 * @var array */
 	protected $objects = [];
 
 	/**
 	 * Constructor
-	 * @param string $namespace Context namespace
-	 * @throws ContextException
+	 * @param Container $Container
+	 * @param EventDispatcher $EventDispatcher
 	 */
-	function __construct($namespace) {
-		$this->_ = $namespace.'.Context';
-		$this->namespace = $namespace;
-		// parse YAML
-		ContextYamlParser::parse($this->namespace, $this->includedNamespaces, $this->id2classMap, $this->listeners);
-		// create Container
-		$Container = new Container($namespace, $this->includedNamespaces);
-		sys::cache('sys')->set($namespace.'.Container', $Container);
-		$this->__wakeup();
+	function __construct(Container $Container, EventDispatcher $EventDispatcher) {
+		$this->Container = $Container;
+		$this->EventDispatcher = $EventDispatcher;
 	}
 
-	function __sleep() {
-		return ['_', 'id2classMap', 'includedNamespaces', 'listeners', 'namespace'];
+	/**
+	 * @return Container
+	 */
+	function container(): Container {
+		return $this->Container;
 	}
 
-	function __wakeup() {
-		foreach ($this->listeners as $eventName => $eventListeners) {
-			foreach ($eventListeners as $priority => $listeners) {
-				foreach ($listeners as $callback) {
-					sys::listen($eventName, $callback, $priority);
-				}
-			}
+	/**
+	 * Initialize namespace
+	 * @param string $namespace Context namespace
+	 * @throws EventDispatcherException
+	 * @throws ContainerException
+	 */
+	function init($namespace) {
+		if(in_array($namespace, $this->namespaces)) return;
+		$this->namespaces[] = $namespace;
+		if(!$context = sys::cache('sys')->get($namespace.'#context')) {
+			$context['container'] = ContainerYamlParser::parseNamespace($namespace);
+			$context['events'] = EventYamlParser::parseNamespace($namespace);
+			sys::cache('sys')->set($namespace.'#context', $context);
 		}
+		$this->Container->init($namespace, $context['container']);
+		$this->EventDispatcher->init($namespace, $context['events']);
 	}
 
 	/**
@@ -97,47 +80,31 @@ class Context {
 	 * @param string $class class/interface that object must extend/implement (optional)
 	 * @return boolean
 	 */
-	function has($id, $class=null) {
-		return ( isset($this->id2classMap[$id]) && ( is_null($class) || (in_array($class,$this->id2classMap[$id])) ) ) ? true : false;
+	function has($id, $class=null): bool {
+		return $this->Container->has($id, $class);
 	}
 
 	/**
 	 * Get an object Proxy
-	 * @param string $id object identifier
-	 * @param string $class required object class
+	 * @param string $id           object identifier
+	 * @param string $class        required object class
 	 * @param integer $failureMode failure mode when the object does not exist
 	 * @return object
 	 * @throws ContextException
+	 * @throws EventDispatcherException
 	 */
 	function get($id, $class=null, $failureMode=self::FAILURE_EXCEPTION) {
 		sys::trace(LOG_DEBUG, T_DEPINJ, 'GET '.$id, null, $this->_);
 		if(isset($this->objects[$id]) && (is_null($class) || $this->objects[$id] instanceof $class)) return $this->objects[$id];
 		try {
-			$Obj = null;
+			sys::context()->init(substr($id, 0, strrpos($id, '.')));
 			if($this->has($id, $class)) {
-				$Obj = new CoreProxy($id);
-			} else {
-				$ctxNamespace = null;
-				foreach($this->includedNamespaces as $namespace) {
-					if(strpos($id, $namespace)===0) $ctxNamespace = $namespace;
-				}
-				if(!is_null($ctxNamespace))
-					$Obj = self::factory($ctxNamespace)->get($id, $class);
-			}
-			if(is_null($Obj)) throw new ContextException(1, [$this->_, $id]);
-			$this->objects[$id] = $Obj;
-			return $Obj;
+				return $this->objects[$id] = new CoreProxy($id);
+			} elseif($failureMode==self::FAILURE_SILENT) return null;
+			else throw new ContextException(1, [$this->_, $id]);
 		} catch(ContainerException $Ex) {
 			if($failureMode==self::FAILURE_SILENT) return null;
 			throw new ContextException($Ex->getCode(), $Ex->getMessage());
 		}
-	}
-
-	/**
-	 * return Dependency Injector Container
-	 * @return \metadigit\core\container\Container
-	 */
-	function getContainer() {
-		return sys::cache('sys')->get($this->namespace.'.Container');
 	}
 }
