abc7603e4ad3ff8b338a4a5299e6fdade8be1904
Daniele Sciacchitano <dan@metadigit.it>
2017-12-26 20:35:41 +0100

    minor [phar-stub] include CacheInterface, Tracer

--- src/phar-stub.php
+++ src/phar-stub.php
@@ -4,4 +4,6 @@ include 'phar://metadigit-core.phar/constants.inc';
 include 'phar://metadigit-core.phar/CoreProxy.php';
 include 'phar://metadigit-core.phar/CoreTrait.php';
 include 'phar://metadigit-core.phar/sys.php';
+include 'phar://metadigit-core.phar/cache/CacheInterface.php';
+include 'phar://metadigit-core.phar/trace/Tracer.php';
 __HALT_COMPILER();
