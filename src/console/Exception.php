039f88de3d37d95710841238d88199820f1b5990
Daniele Sciacchitano <dan@metadigit.it>
2018-10-14 14:44:44 +0200

    header & copyright update

--- src/console/Exception.php
+++ src/console/Exception.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Metadigit Core PHP Framework
- * @link http://github.com/Metadigit/Core
- * @copyright Copyright (c) 2004-2014 Daniele Sciacchitano <dan@metadigit.it>
+ * Renovant Technology Core PHP Framework
+ * @link https://github.com/RenovantTech/Core
+ * @copyright Copyright (c) 2004-2018 Daniele Sciacchitano
  * @license New BSD License
  */
 namespace metadigit\core\console;
