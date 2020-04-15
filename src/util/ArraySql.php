bcc29e38bfd47bb2b0bc12e657084c81cb83fb07
Daniele Sciacchitano <dan@renovant.tech>
2020-04-15 12:54:18 +0200

    feature [util] new ArraySql class

---
+++ src/util/ArraySql.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Renovant Technology Core PHP Framework
+ * @link https://github.com/RenovantTech/Core
+ * @copyright Copyright (c) 2004-2018 Daniele Sciacchitano
+ * @license New BSD License
+ */
+namespace renovant\core\util;
+/**
+ * Array query language
+ * @author Daniele Sciacchitano <dan@renovant.tech>
+ */
+class ArraySql {
+
+	protected $data;
+
+	function __construct(&$data) {
+		$this->data =& $data;
+	}
+
+	function orderExp($orderExp=null) {
+		if(empty($orderExp)) return $this;
+		$expArray = explode('|', $orderExp);
+		$sorts = $indexes = [];
+		foreach ($expArray as $exp) {
+			list($col, $sort) = explode('.', $exp);
+			$sorts[] = (strtoupper($sort) == 'ASC') ? SORT_ASC : SORT_DESC;
+			$indexes[]  = array_column($this->data, $col);
+		}
+		switch (count($expArray)) {
+			case 1: array_multisort($indexes[0], $sorts[0], $this->data); break;
+			case 2: array_multisort($indexes[0], $sorts[0], $indexes[1], $sorts[1], $this->data); break;
+			case 3: array_multisort($indexes[0], $sorts[0], $indexes[1], $sorts[1], $indexes[2], $sorts[2], $this->data); break;
+			case 4: array_multisort($indexes[0], $sorts[0], $indexes[1], $sorts[1], $indexes[2], $sorts[2], $indexes[3], $sorts[3], $this->data); break;
+		}
+		return $this;
+	}
+
+	function page($page=null, $pageSize=null) {
+		array_slice($this->data, ($page-1)*$pageSize, $pageSize);
+		return $this;
+	}
+}
