0d86700c57ec753f2881b045762c3f74cd5b8fee
Daniele Sciacchitano <dan@renovant.tech>
2021-09-02 11:49:49 +0200

    feature/refactor [console] CmdManager: start()=>exec() with delayed execution on HTTP SAPI

--- src/console/CmdManager.php
+++ src/console/CmdManager.php
@@ -16,15 +16,17 @@ use renovant\core\sys;
 class CmdManager {
 	use \renovant\core\CoreTrait;
 
-	/**
-	 * CLI command start
-	 * @param string $cmd
-	 * @return integer command process PID
-	 */
-	function start($cmd) {
-		$cmd = CLI_PHP_BIN.' '.CLI_BOOTSTRAP.' '.$cmd;
-		sys::trace(LOG_DEBUG, T_INFO, 'START '.$cmd, null, 'sys.CmdManager');
-		return (int) shell_exec('nohup '.$cmd.' > /dev/null 2>&1 & echo $!');
+	static protected $buffer = [];
+
+	function exec(string $cmd) {
+		if(PHP_SAPI == 'cli') {
+			$exec = CLI_PHP_BIN.' '.CLI_BOOTSTRAP.' '.$cmd;
+			sys::trace(LOG_DEBUG, T_INFO, '[EXEC] '.$cmd, $exec, 'sys.CmdManager');
+			shell_exec('nohup '.$exec.' > /dev/null 2>&1 & echo $!');
+		} else {
+			sys::trace(LOG_DEBUG, T_INFO, '[EXEC on shutdown] '.$cmd, null, 'sys.CmdManager');
+			self::$buffer[] = $cmd;
+		}
 	}
 
 	function stop($cmd) {
@@ -32,4 +34,14 @@ class CmdManager {
 		$pid = file_get_contents($pidLock);
 		posix_kill($pid, SIGTERM);
 	}
+
+	static function shutdown() {
+		foreach (self::$buffer as $cmd) {
+			$exec = CLI_PHP_BIN.' '.CLI_BOOTSTRAP.' '.$cmd;
+			sys::trace(LOG_DEBUG, T_INFO, '[EXEC] '.$cmd, $exec, 'sys.CmdManager');
+			shell_exec('nohup '.$exec.' > /dev/null 2>&1 & echo $!');
+		}
+	}
 }
+if(PHP_SAPI != 'cli')
+	register_shutdown_function(__NAMESPACE__.'\CmdManager::shutdown');
