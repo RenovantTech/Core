193fa33b91d763b71badcaf0beaa247e64ef85a5
Daniele Sciacchitano <dan@renovant.tech>
2021-10-22 00:33:11 +0200

    refactor [console] CmdManager: add SQL store, add method scan()

--- src/console/CmdManager.php
+++ src/console/CmdManager.php
@@ -8,6 +8,32 @@ class CmdManager {
 
 	static protected $buffer = [];
 
+	/** PDO instance ID
+	 * @var string */
+	protected $pdo;
+	/** PDO tables prefix
+	 * @var string */
+	protected $tablePrefix = 'sys_cmd';
+
+	/**
+	 * @param string|null $pdo PDO instance ID, default to "master"
+	 * @param string|null $tablePrefix
+	 */
+	function __construct(?string $pdo='master', ?string $tablePrefix=null) {
+		$prevTraceFn = sys::traceFn('sys.CmdManager');
+		try {
+			if ($pdo) $this->pdo = $pdo;
+			if ($tablePrefix) $this->tablePrefix = $tablePrefix;
+			sys::trace(LOG_DEBUG, T_INFO, 'initialize SQL storage');
+			$PDO = sys::pdo($this->pdo);
+			$driver = $PDO->getAttribute(\PDO::ATTR_DRIVER_NAME);
+			$PDO->exec(str_replace('%table%', $this->tablePrefix, file_get_contents(__DIR__ . '/sql/init-' . $driver . '.sql')
+			));
+		} finally {
+			sys::traceFn($prevTraceFn);
+		}
+	}
+
 	function exec(string $cmd) {
 		if(PHP_SAPI == 'cli') {
 			$exec = CLI_PHP_BIN.' '.CLI_BOOTSTRAP.' '.$cmd;
@@ -23,6 +49,11 @@ class CmdManager {
 		$pidLock = RUN_DIR.str_replace(' ', '-', $cmd).'.pid';
 		$pid = file_get_contents($pidLock);
 		posix_kill($pid, SIGTERM);
+	}
+
+	function scan() {
+		include __DIR__.'/CmdManager.scan.inc';
+		scan($this->pdo, $this->tablePrefix);
 	}
 
 	static function shutdown() {
