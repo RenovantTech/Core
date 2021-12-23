f61951d8ba66c2ca229b113ec9568eee302ae79f
Daniele Sciacchitano <dan@renovant.tech>
2021-12-23 03:24:44 +0100

    refactor [util] DatePeriod static create() method

--- src/util/DatePeriod.php
+++ src/util/DatePeriod.php
@@ -3,14 +3,15 @@ namespace renovant\core\util;
 
 class DatePeriod extends \DatePeriod {
 
-	/**
-	 * @throws \Exception
-	 */
-	static function new($start, $interval, $end): DatePeriod {
+	/**  @throws \Exception */
+	static function create($start, $interval, $end): array {
 		if(is_string($start)) $start = new \DateTime($start);
 		if(is_string($interval)) $interval = new \DateInterval($interval);
 		if(is_string($end)) $end = (new \DateTime($end))->modify('+1 days');
-
-		return new DatePeriod($start, $interval, $end);
+		$Period = new \DatePeriod($start, $interval, $end);
+		$dates = [];
+		foreach ($Period as $date)
+			$dates[] = new Date($date->format('Y-m-d'));
+		return $dates;
 	}
 }
