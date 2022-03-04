14c87b19e00db63baa09e7e3d5c950da4ca84557
Daniele Sciacchitano <dan@renovant.tech>
2022-03-04 03:47:58 +0100

    feature [util] Crypto: optional param bin2hex/hex2bin

--- src/util/crypto/Crypto.php
+++ src/util/crypto/Crypto.php
@@ -6,9 +6,7 @@ class Crypto {
 	const KEY_FILE = DATA_DIR.'crypto.key';
 	static protected $key = null;
 
-	/**
-	 * @throws \Exception
-	 */
+	/** @throws \Exception */
 	static protected function init() {
 		if(!is_null(self::$key)) return;
 
@@ -23,28 +21,29 @@ class Crypto {
 	/**
 	 * Encrypt data (with automatic object JSON encoding)
 	 * @param mixed $data
+	 * @param bool $bin2hex
 	 * @return string
 	 * @throws \SodiumException
 	 * @throws \Exception
 	 */
-	static function encrypt($data) {
+	static function encrypt($data, bool $bin2hex=false) {
 		self::init();
 		$data = serialize($data);
 		$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
 		$cipherText = sodium_crypto_secretbox($data, $nonce, self::$key);
-		return sodium_bin2hex($nonce.$cipherText);
+		return $bin2hex ? sodium_bin2hex($nonce.$cipherText) : $nonce.$cipherText;
 	}
 
 	/**
 	 * Decrypt data (with automatic object JSON decoding)
 	 * @param $cryptData
+	 * @param bool $hex2bin
 	 * @return mixed
 	 * @throws \SodiumException
-	 * @throws \Exception
 	 */
-	static function decrypt($cryptData) {
+	static function decrypt($cryptData, bool $hex2bin=false) {
 		self::init();
-		$data = sodium_hex2bin($cryptData);
+		$data = $hex2bin ? sodium_hex2bin($cryptData) : $cryptData;
 		$nonce = mb_substr($data, 0, SODIUM_CRYPTO_STREAM_NONCEBYTES, '8bit');
 		$cipherText = mb_substr($data, SODIUM_CRYPTO_STREAM_NONCEBYTES, null, '8bit');
 		$data = sodium_crypto_secretbox_open($cipherText, $nonce, self::$key);
