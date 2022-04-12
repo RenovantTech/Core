ba0d478eea97beea4adb065a8a3596f58c646997
Daniele Sciacchitano <dan@renovant.tech>
2022-04-12 02:35:02 +0200

    fix [http] ActionController: call AUTHZ with $args

--- src/http/controller/ActionController.php
+++ src/http/controller/ActionController.php
@@ -64,7 +64,7 @@ abstract class ActionController implements \renovant\core\http\ControllerInterfa
 		$prevTraceFn = sys::traceFn($this->_.'->'.$action);
 		try {
 			// AUTHZ check
-			method_exists($this, '_authz') and $this->_authz($action);
+			method_exists($this, '_authz') and $this->_authz($action, $args);
 			sys::trace(LOG_DEBUG, T_INFO);
 			call_user_func_array([$this, $action], $args);
 			$this->postHandle($Req, $Res);
