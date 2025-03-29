40b6c4583c808389a9237aca99fed33286a046d3
Daniele Sciacchitano <dan@renovant.tech>
2025-03-29 17:26:14 +0100

    feature [http] App modules mapping with proper context initialization

--- tests/http/AppTest.php
+++ tests/http/AppTest.php
@@ -13,18 +13,37 @@ class AppTest extends \PHPUnit\Framework\TestCase {
 	 */
 	public function testRun() {
 		$App = sys::context()->get('test.http.AppHTTP');
+		$Res = new Response();
 		//		$this->assertInstanceOf(App::class, $App);
 
 		$_SERVER['SERVER_ADDR'] = 'example.com';
 		$_SERVER['SERVER_PORT'] = 443;
-		$_SERVER['REQUEST_URI'] = '/api/bar/';
-
-		$Req = new Request();
-		$Res = new Response();
+		$_SERVER['REQUEST_URI'] = '/api/mod2/foo/bar';
+		$Req                    = new Request();
 		$this->assertNull($App->run($Req, $Res));
 		$this->assertSame('TEST-APP', $Req->getAttribute('APP'));
-		$this->assertSame('MOD1', $Req->getAttribute('APP_MOD'));
-		$this->assertSame('test.http', $Req->getAttribute('APP_MOD_NAMESPACE'));
-		$this->assertSame('/api/bar/', $Req->getAttribute('APP_MOD_URI'));
+		$this->assertSame('MOD2', $Req->getAttribute('APP_MOD'));
+		$this->assertSame('test.http.mod2', $Req->getAttribute('APP_MOD_CONTEXT'));
+		$this->assertSame('/foo/bar', $Req->getAttribute('APP_MOD_URI'));
+
+		$_SERVER['SERVER_ADDR'] = 'example.com';
+		$_SERVER['SERVER_PORT'] = 443;
+		$_SERVER['REQUEST_URI'] = '/api/mod3/foo/bar';
+		$Req                    = new Request();
+		$this->assertNull($App->run($Req, $Res));
+		$this->assertSame('TEST-APP', $Req->getAttribute('APP'));
+		$this->assertSame('MOD3', $Req->getAttribute('APP_MOD'));
+		$this->assertSame('test.http', $Req->getAttribute('APP_MOD_CONTEXT'));
+		$this->assertSame('/foo/bar', $Req->getAttribute('APP_MOD_URI'));
+
+		$_SERVER['SERVER_ADDR'] = 'example.com';
+		$_SERVER['SERVER_PORT'] = 443;
+		$_SERVER['REQUEST_URI'] = '/api/bar/';
+		$Req                    = new Request();
+		$this->assertNull($App->run($Req, $Res));
+		$this->assertSame('TEST-APP', $Req->getAttribute('APP'));
+		$this->assertSame('', $Req->getAttribute('APP_MOD'));
+		$this->assertSame('test.http', $Req->getAttribute('APP_MOD_CONTEXT'));
+		$this->assertSame('/bar/', $Req->getAttribute('APP_MOD_URI'));
 	}
 }
